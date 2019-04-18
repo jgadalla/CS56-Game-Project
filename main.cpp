cd7be72be08bf1cc764dc07a7f8bd9133c1036e4
jgadalla <35013480+jgadalla@users.noreply.github.com>
2019-04-18 11:33:48 -0700

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@ and may not be redistributed without written permission.*/
 #include <SDL2_image/SDL_image.h>
 #include <stdio.h>
 #include <string>
+#include <iostream>
 
 //Screen dimension constants
 const int SCREEN_WIDTH = 792;
@@ -30,10 +31,28 @@ SDL_Window* gWindow = NULL;
 SDL_Surface* gScreenSurface = NULL;
 
 //Current displayed PNG image
-SDL_Surface* gPNGSurface = NULL;
+SDL_Surface* gStartSurface = NULL;
+SDL_Surface* gGameSurface = NULL;
 
 //Current renderer for shapes
 SDL_Renderer* gRenderer = NULL;
+
+int check_click_in_rect(int x, int y, SDL_Rect *rect)
+{
+    /* Check X coordinate is within rectangle range */
+    if (x >= rect->x && x < (rect->x + rect->w))
+    {
+        /* Check Y coordinate is within rectangle range */
+        if (y >= rect->y && y < (rect->y + rect->h))
+        {
+            /* X and Y is inside the rectangle */
+            return 1;
+        }
+    }
+
+    /* X or Y is outside the rectangle */
+    return 0;
+}
 
 bool init()
 {
@@ -93,21 +112,28 @@ bool loadMedia()
 	bool success = true;
 
 	//Load PNG surface
-	gPNGSurface = loadSurface( "Start-Screen.png" );
-	if( gPNGSurface == NULL )
+	gStartSurface = loadSurface( "Start-Screen.png" );
+	if( gStartSurface== NULL )
 	{
 		printf( "Failed to load PNG image!\n" );
 		success = false;
 	}
-
+	gGameSurface = loadSurface( "Game-Screen.png" );
+	if( gGameSurface== NULL )
+	{
+		printf( "Failed to load PNG image!\n" );
+		success = false;
+	}
 	return success;
 }
 
 void close()
 {
 	//Free loaded image
-	SDL_FreeSurface( gPNGSurface );
-	gPNGSurface = NULL;
+	SDL_FreeSurface( gGameSurface );
+	gGameSurface = NULL;
+	SDL_FreeSurface( gStartSurface );
+	gStartSurface = NULL;
 
 	SDL_DestroyRenderer( gRenderer );
 	gRenderer = NULL;
@@ -166,6 +192,8 @@ int main( int argc, char* args[] )
 		{	
 			//Main loop flag
 			bool quit = false;
+			bool start_screen = true;
+
 
 			//Event handler
 			SDL_Event e;
@@ -173,6 +201,16 @@ int main( int argc, char* args[] )
 			//While application is running
 			while( !quit )
 			{
+				printf("Start of game loop\n");
+
+				SDL_Rect* start_button = new SDL_Rect();
+				start_button->x = 286;
+				start_button->y = 132;
+				start_button->w = 208;
+				start_button->h = 57;
+				SDL_RenderDrawRect(gRenderer, start_button);
+
+				printf("Drawn Rectangle\n");
 				//Handle events on queue
 				while( SDL_PollEvent( &e ) != 0 )
 				{
@@ -183,16 +221,20 @@ int main( int argc, char* args[] )
 					}
 				}
 
-				SDL_Rect* start_button = new SDL_Rect();
-				start_button->x = 286;
-				start_button->y = 132;
-				start_button->w = 208;
-				start_button->h = 57;
-				SDL_RenderDrawRect(gRenderer, start_button);
+				if (e.type == SDL_MOUSEBUTTONDOWN || e.type == SDL_MOUSEBUTTONUP){
+					int x, y;
+					SDL_GetMouseState(&x, &y);
+					if(e.type == SDL_MOUSEBUTTONUP && check_click_in_rect(x,y,start_button)){
+						start_screen = false;
+					}
+				}
 
-				//Apply the PNG image
-				SDL_BlitSurface( gPNGSurface, NULL, gScreenSurface, NULL );
-			
+				if(start_screen){
+					SDL_BlitSurface( gStartSurface, NULL, gScreenSurface, NULL );
+				}
+				else{
+					SDL_BlitSurface( gGameSurface, NULL, gScreenSurface, NULL );
+				}
 				//Update the surface
 				SDL_UpdateWindowSurface( gWindow );
 				SDL_RenderPresent(gRenderer);
