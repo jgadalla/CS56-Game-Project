7f03422b7852e502db2c1589e5bb132b8b07bbec
CS70 Starfish <JUSTIN@JUSTIN-GADALLAs-MacBook-Pro-2.local>
2019-04-25 11:08:17 -0700

    Working button click

--- main.cpp
+++ main.cpp
@@ -7,10 +7,43 @@ and may not be redistributed without written permission.*/
 #include <stdio.h>
 #include <string>
 #include <iostream>
+#include "Button.hpp"
 
 //Screen dimension constants
 const int SCREEN_WIDTH = 792;
 const int SCREEN_HEIGHT = 469;
+
+//Texture wrapper class
+class LTexture
+{
+  public:
+	//Initializes variables
+	LTexture();
+
+	//Deallocates memory
+	~LTexture();
+
+	//Loads image at specified path
+	bool loadFromFile(std::string path);
+
+	//Deallocates texture
+	void free();
+
+	//Renders texture at given point
+	void render(int x, int y);
+
+	//Gets image dimensions
+	int getWidth();
+	int getHeight();
+
+  private:
+	//The actual hardware texture
+	SDL_Texture *mTexture;
+
+	//Image dimensions
+	int mWidth;
+	int mHeight;
+};
 
 //Starts up SDL and creates window
 bool init();
@@ -22,36 +55,122 @@ bool loadMedia();
 void close();
 
 //Loads individual image
-SDL_Surface* loadSurface( std::string path );
+SDL_Surface *loadSurface(std::string path);
 
 //The window we'll be rendering to
-SDL_Window* gWindow = NULL;
-	
+SDL_Window *gWindow = NULL;
+
 //The surface contained by the window
-SDL_Surface* gScreenSurface = NULL;
+//SDL_Surface* gScreenSurface = NULL;
 
 //Current displayed PNG image
-SDL_Surface* gStartSurface = NULL;
-SDL_Surface* gGameSurface = NULL;
+// SDL_Surface* gStartSurface = NULL;
+// SDL_Surface* gGameSurface = NULL;
 
 //Current renderer for shapes
-SDL_Renderer* gRenderer = NULL;
+SDL_Renderer *gRenderer = NULL;
+
+//Scene textures
+LTexture startTexture;
+LTexture gameTexture;
+
+LTexture::LTexture()
+{
+	//Initialize
+	mTexture = NULL;
+	mWidth = 0;
+	mHeight = 0;
+}
+
+LTexture::~LTexture()
+{
+	//Deallocate
+	free();
+}
+
+bool LTexture::loadFromFile(std::string path)
+{
+	//Get rid of preexisting texture
+	free();
+
+	//The final texture
+	SDL_Texture *newTexture = NULL;
+
+	//Load image at specified path
+	SDL_Surface *loadedSurface = IMG_Load(path.c_str());
+	if (loadedSurface == NULL)
+	{
+		printf("Unable to load image %s! SDL_image Error: %s\n", path.c_str(), IMG_GetError());
+	}
+	else
+	{
+
+		//Create texture from surface pixels
+		newTexture = SDL_CreateTextureFromSurface(gRenderer, loadedSurface);
+		if (newTexture == NULL)
+		{
+			printf("Unable to create texture from %s! SDL Error: %s\n", path.c_str(), SDL_GetError());
+		}
+		else
+		{
+			//Get image dimensions
+			mWidth = loadedSurface->w;
+			mHeight = loadedSurface->h;
+		}
+
+		//Get rid of old loaded surface
+		SDL_FreeSurface(loadedSurface);
+	}
+
+	//Return success
+	mTexture = newTexture;
+	return mTexture != NULL;
+}
+
+void LTexture::free()
+{
+	//Free texture if it exists
+	if (mTexture != NULL)
+	{
+		SDL_DestroyTexture(mTexture);
+		mTexture = NULL;
+		mWidth = 0;
+		mHeight = 0;
+	}
+}
+
+void LTexture::render(int x, int y)
+{
+	//Set rendering space and render to screen
+	SDL_Rect renderQuad = {x, y, mWidth, mHeight};
+	SDL_RenderCopy(gRenderer, mTexture, NULL, &renderQuad);
+}
+
+int LTexture::getWidth()
+{
+	return mWidth;
+}
+
+int LTexture::getHeight()
+{
+	return mHeight;
+}
 
 int check_click_in_rect(int x, int y, SDL_Rect *rect)
 {
-    /* Check X coordinate is within rectangle range */
-    if (x >= rect->x && x < (rect->x + rect->w))
-    {
-        /* Check Y coordinate is within rectangle range */
-        if (y >= rect->y && y < (rect->y + rect->h))
-        {
-            /* X and Y is inside the rectangle */
-            return 1;
-        }
-    }
+	/* Check X coordinate is within rectangle range */
+	if (x >= rect->x && x < (rect->x + rect->w))
+	{
+		/* Check Y coordinate is within rectangle range */
+		if (y >= rect->y && y < (rect->y + rect->h))
+		{
+			/* X and Y is inside the rectangle */
+			return 1;
+		}
+	}
 
-    /* X or Y is outside the rectangle */
-    return 0;
+	/* X or Y is outside the rectangle */
+	return 0;
 }
 
 bool init()
@@ -60,23 +179,30 @@ bool init()
 	bool success = true;
 
 	//Initialize SDL
-	if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
+	if (SDL_Init(SDL_INIT_VIDEO) < 0)
 	{
-		printf( "SDL could not initialize! SDL Error: %s\n", SDL_GetError() );
+		printf("SDL could not initialize! SDL Error: %s\n", SDL_GetError());
 		success = false;
 	}
 	else
 	{
-		//Create window
-		gWindow = SDL_CreateWindow( "Sixth Street Scramble", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
-		if( gWindow == NULL )
+
+		//Set texture filtering to linear
+		if (!SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "1"))
 		{
-			printf( "Window could not be created! SDL Error: %s\n", SDL_GetError() );
+			printf("Warning: Linear texture filtering not enabled!");
+		}
+
+		//Create window
+		gWindow = SDL_CreateWindow("Sixth Street Scramble", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+		if (gWindow == NULL)
+		{
+			printf("Window could not be created! SDL Error: %s\n", SDL_GetError());
 			success = false;
 		}
 		else
 		{
-			gRenderer = SDL_CreateRenderer(gWindow, -1, SDL_RENDERER_ACCELERATED);
+			gRenderer = SDL_CreateRenderer(gWindow, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
 			if (gRenderer == NULL)
 			{
 				printf("Renderer could not be created! SDL Error: %s\n", SDL_GetError());
@@ -86,19 +212,14 @@ bool init()
 			{
 				//Initialize renderer color
 				SDL_SetRenderDrawColor(gRenderer, 0xFF, 0xFF, 0xFF, 0xFF);
-			}
 
-			//Initialize PNG loading
-			int imgFlags = IMG_INIT_PNG;
-			if( !( IMG_Init( imgFlags ) & imgFlags ) )
-			{
-				printf( "SDL_image could not initialize! SDL_image Error: %s\n", IMG_GetError() );
-				success = false;
-			}
-			else
-			{
-				//Get window surface
-				gScreenSurface = SDL_GetWindowSurface( gWindow );
+				//Initialize PNG loading
+				int imgFlags = IMG_INIT_PNG;
+				if (!(IMG_Init(imgFlags) & imgFlags))
+				{
+					printf("SDL_image could not initialize! SDL_image Error: %s\n", IMG_GetError());
+					success = false;
+				}
 			}
 		}
 	}
@@ -111,17 +232,17 @@ bool loadMedia()
 	//Loading success flag
 	bool success = true;
 
-	//Load PNG surface
-	gStartSurface = loadSurface( "Start-Screen.png" );
-	if( gStartSurface== NULL )
+	//Load PNG surface into textures
+
+	if (!startTexture.loadFromFile("PNGs/Start-Screen.png"))
 	{
-		printf( "Failed to load PNG image!\n" );
+		printf("Failed to load Foo' texture image!\n");
 		success = false;
 	}
-	gGameSurface = loadSurface( "Game-Screen.png" );
-	if( gGameSurface== NULL )
+
+	if (!gameTexture.loadFromFile("PNGs/Game-Screen.png"))
 	{
-		printf( "Failed to load PNG image!\n" );
+		printf("Failed to load background texture image!\n");
 		success = false;
 	}
 	return success;
@@ -130,115 +251,124 @@ bool loadMedia()
 void close()
 {
 	//Free loaded image
-	SDL_FreeSurface( gGameSurface );
-	gGameSurface = NULL;
-	SDL_FreeSurface( gStartSurface );
-	gStartSurface = NULL;
-
-	SDL_DestroyRenderer( gRenderer );
-	gRenderer = NULL;
+	gameTexture.free();
+	startTexture.free();
 
 	//Destroy window
-	SDL_DestroyWindow( gWindow );
+	SDL_DestroyRenderer(gRenderer);
+	SDL_DestroyWindow(gWindow);
 	gWindow = NULL;
+	gRenderer = NULL;
 
 	//Quit SDL subsystems
 	IMG_Quit();
 	SDL_Quit();
 }
+// ------- Done withing the texture class --------
+// SDL_Surface* loadSurface( std::string path )
+// {
+// 	//The final optimized image
+// 	SDL_Surface* optimizedSurface = NULL;
 
-SDL_Surface* loadSurface( std::string path )
-{
-	//The final optimized image
-	SDL_Surface* optimizedSurface = NULL;
+// 	//Load image at specified path
+// 	SDL_Surface* loadedSurface = IMG_Load( path.c_str() );
+// 	if( loadedSurface == NULL )
+// 	{
+// 		printf( "Unable to load image %s! SDL_image Error: %s\n", path.c_str(), IMG_GetError() );
+// 	}
+// 	else
+// 	{
+// 		//Convert surface to screen format
+// 		optimizedSurface = SDL_ConvertSurface( loadedSurface, gScreenSurface->format, 0 );
+// 		if( optimizedSurface == NULL )
+// 		{
+// 			printf( "Unable to optimize image %s! SDL Error: %s\n", path.c_str(), SDL_GetError() );
+// 		}
 
-	//Load image at specified path
-	SDL_Surface* loadedSurface = IMG_Load( path.c_str() );
-	if( loadedSurface == NULL )
-	{
-		printf( "Unable to load image %s! SDL_image Error: %s\n", path.c_str(), IMG_GetError() );
-	}
-	else
-	{
-		//Convert surface to screen format
-		optimizedSurface = SDL_ConvertSurface( loadedSurface, gScreenSurface->format, 0 );
-		if( optimizedSurface == NULL )
-		{
-			printf( "Unable to optimize image %s! SDL Error: %s\n", path.c_str(), SDL_GetError() );
-		}
+// 		//Get rid of old loaded surface
+// 		SDL_FreeSurface( loadedSurface );
+// 	}
 
-		//Get rid of old loaded surface
-		SDL_FreeSurface( loadedSurface );
-	}
+// 	return optimizedSurface;
+// }
+//--------------
 
-	return optimizedSurface;
-}
-
-int main( int argc, char* args[] )
+int main(int argc, char *args[])
 {
 	//Start up SDL and create window
-	if( !init() )
+	if (!init())
 	{
-		printf( "Failed to initialize!\n" );
+		printf("Failed to initialize!\n");
 	}
 	else
 	{
 		//Load media
-		if( !loadMedia() )
+		if (!loadMedia())
 		{
-			printf( "Failed to load media!\n" );
+			printf("Failed to load media!\n");
 		}
 		else
-		{	
+		{
 			//Main loop flag
 			bool quit = false;
 			bool start_screen = true;
+			// SDL_Rect *start_button = new SDL_Rect();
+			// start_button->x = 286;
+			// start_button->y = 132;
+			// start_button->w = 208;
+			// start_button->h = 57;
 
+			Button* start_button = new Button(286, 132);
 
 			//Event handler
 			SDL_Event e;
 
 			//While application is running
-			while( !quit )
+			while (!quit)
 			{
-				printf("Start of game loop\n");
+				start_button->render(gRenderer);
 
-				SDL_Rect* start_button = new SDL_Rect();
-				start_button->x = 286;
-				start_button->y = 132;
-				start_button->w = 208;
-				start_button->h = 57;
-				SDL_RenderDrawRect(gRenderer, start_button);
-
-				printf("Drawn Rectangle\n");
 				//Handle events on queue
-				while( SDL_PollEvent( &e ) != 0 )
+				while (SDL_PollEvent(&e) != 0)
 				{
 					//User requests quit
-					if( e.type == SDL_QUIT )
+					if (e.type == SDL_QUIT)
 					{
 						quit = true;
 					}
-				}
 
-				if (e.type == SDL_MOUSEBUTTONDOWN || e.type == SDL_MOUSEBUTTONUP){
-					int x, y;
-					SDL_GetMouseState(&x, &y);
-					if(e.type == SDL_MOUSEBUTTONUP && check_click_in_rect(x,y,start_button)){
-						start_screen = false;
+					if (e.type == SDL_MOUSEBUTTONDOWN || e.type == SDL_MOUSEBUTTONUP)
+					{
+						int x, y;
+						SDL_GetMouseState(&x, &y);
+						SDL_Rect currRect = start_button->getRect();
+						if (e.type == SDL_MOUSEBUTTONUP && check_click_in_rect(x, y, &currRect))
+						{
+							start_screen = false;
+						}
 					}
 				}
 
-				if(start_screen){
-					SDL_BlitSurface( gStartSurface, NULL, gScreenSurface, NULL );
+				//Clear screen
+				SDL_SetRenderDrawColor(gRenderer, 0xFF, 0xFF, 0xFF, 0xFF);
+				SDL_RenderClear(gRenderer);
+
+				if (start_screen)
+				{
+					// SDL_BlitSurface( gStartSurface, NULL, gScreenSurface, NULL );
+					startTexture.render(0, 0);
 				}
-				else{
-					SDL_BlitSurface( gGameSurface, NULL, gScreenSurface, NULL );
+				else
+				{
+					// SDL_BlitSurface( gGameSurface, NULL, gScreenSurface, NULL );
+					gameTexture.render(0, 0);
 				}
 				//Update the surface
-				SDL_UpdateWindowSurface( gWindow );
+				//SDL_UpdateWindowSurface( gWindow );
 				SDL_RenderPresent(gRenderer);
 			}
+			delete start_button;
+			start_button = NULL;
 		}
 	}
 
